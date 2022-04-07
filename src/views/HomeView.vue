6a685f43721f2ef2026c10be21e566e998a8d374
Volkan TAŞCI <me@volkantasci.com>
2022-04-07 12:09:07 +0300

    Mobile design applied

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,42 +2,66 @@
   <div class="mt-7">
     <v-container>
 
-      <v-text-field
-        label="Görev"
-        dense
-        v-model="todoText"
-        outlined
-        light
+      <v-row>
+        <v-col cols="6">
+          <v-text-field
+            label="Title"
+            dense
+            v-model="todoTitle"
+            outlined
+            light
+            color="secondary"
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="6">
+          <v-autocomplete
+            clearable
+            :items="categories"
+            item-text="name"
+            item-value="id"
+            label="Category"
+            v-model="todoCategory"
+            dense
+            outlined
+            color="secondary"
+          />
+        </v-col>
+      </v-row>
+          <v-text-field
+              label="Content"
+              v-model="todoContent"
+              color="secondary"
+              dense
+              outlined
+          />
+      <v-btn
+        block
         color="secondary"
+        @click="addToList"
       >
-        <template v-slot:append>
-          <v-fade-transition>
-            <v-icon
-              v-if="todoText"
-              @click="addToList"
-            >
-              mdi-plus-circle
-            </v-icon>
-          </v-fade-transition>
-        </template>
-      </v-text-field>
-      <v-autocomplete
-          :items="categories"
-          item-text="name"
-          item-value="id"
-          dense
-          outlined
-          color="secondary"
-      />
-      <div class="mt-8">
+        <v-icon left>mdi-arrow-down</v-icon>
+        <span>Create</span>
+      </v-btn>
+      <v-divider class="mt-3"/>
+      <div class="mt-8 text-center">
+        <h3 class="blue-grey--text">Created Tasks</h3>
+        <div v-if="todos.length < 1" class="text-center">
+          <span>You don't have any tasks!</span>
+        </div>
         <v-card
           hover
           v-for="todo in todos"
-          :key="todo.id" class="mb-2"
+          :key="todo.id" class="mb-2 mt-5"
           shaped
           dark
           color="secondary lighten-1"
         >
+          <v-card-title class="subtitle-1 orange--text" :class="classForTodoText(todo)">
+            <span>{{ todo.title }}</span>
+            <v-spacer />
+            <span>{{ getCategoryName(todo.category) }}</span>
+          </v-card-title>
           <v-card-text :class="classForTodoText(todo)" style="font-size: 105% !important;">{{
               todo.content
             }}
@@ -84,7 +108,9 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'HomeView',
   data: () => ({
-    todoText: ''
+    todoTitle: '',
+    todoContent: '',
+    todoCategory: null
   }),
 
   computed: {
@@ -100,6 +126,9 @@ export default {
       addTodo: 'addTodo',
       deleteTodo: 'deleteTodo'
     }),
+    getCategoryName (categoryId) {
+      return this.categories.find(c => c.id === categoryId).name
+    },
     classForTodoText (todo) {
       if (todo.completed) {
         return 'text-decoration-line-through'
@@ -108,11 +137,13 @@ export default {
       }
     },
     addToList () {
-      if (this.todoText) {
+      if (this.todoContent) {
         this.addTodo({
           todo: {
             id: this.nextId,
-            content: this.todoText,
+            content: this.todoContent,
+            title: this.todoTitle,
+            category: this.todoCategory,
             completed: false
           }
         })
